feb749cf9f0a2f68dbf59734460a5f956081f954
kotzimop <kotzimop@gmail.com>
2024-01-07 13:58:39 +0200

    first commit

--- hooks/use-login.ts
+++ hooks/use-login.ts
@@ -1,48 +1,50 @@
-import { useState, ChangeEvent, FormEvent } from 'react';
-import { useRouter } from 'next/navigation';
-import { useAppDispatch } from '@/redux/hooks';
-import { useLoginMutation } from '@/redux/features/authApiSlice';
-import { setAuth } from '@/redux/features/authSlice';
-import { toast } from 'react-toastify';
+import { useState, ChangeEvent, FormEvent } from "react";
+import { useRouter } from "next/navigation";
+import { useAppDispatch } from "@/redux/hooks";
+import { useLoginMutation } from "@/redux/features/authApiSlice";
+import { setAuth } from "@/redux/features/authSlice";
+import { toast } from "react-toastify";
 
 export default function useLogin() {
-	const router = useRouter();
-	const dispatch = useAppDispatch();
-	const [login, { isLoading }] = useLoginMutation();
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const [login, { isLoading }] = useLoginMutation();
 
-	const [formData, setFormData] = useState({
-		email: '',
-		password: '',
-	});
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-	const { email, password } = formData;
+  const { email, password } = formData;
 
-	const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = event.target;
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
 
-		setFormData({ ...formData, [name]: value });
-	};
+    setFormData({ ...formData, [name]: value });
+  };
 
-	const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-		login({ email, password })
-			.unwrap()
-			.then(() => {
-				dispatch(setAuth());
-				toast.success('Logged in');
-				router.push('/dashboard');
-			})
-			.catch(() => {
-				toast.error('Failed to log in');
-			});
-	};
+    login({ email, password })
+      .unwrap()
+      .then(() => {
+        dispatch(setAuth());
+        toast.success("Logged in");
+        router.push("/dashboard");
+      })
+      .catch((error) => {
+        if (error) {
+          toast.error(`${Object.values(error.data)[0]}`);
+        }
+      });
+  };
 
-	return {
-		email,
-		password,
-		isLoading,
-		onChange,
-		onSubmit,
-	};
+  return {
+    email,
+    password,
+    isLoading,
+    onChange,
+    onSubmit,
+  };
 }
--- hooks/use-reset-password-confirm.ts
+++ hooks/use-reset-password-confirm.ts
@@ -1,46 +1,48 @@
-import { useState, ChangeEvent, FormEvent } from 'react';
-import { useRouter } from 'next/navigation';
-import { useResetPasswordConfirmMutation } from '@/redux/features/authApiSlice';
-import { toast } from 'react-toastify';
+import { useState, ChangeEvent, FormEvent } from "react";
+import { useRouter } from "next/navigation";
+import { useResetPasswordConfirmMutation } from "@/redux/features/authApiSlice";
+import { toast } from "react-toastify";
 
 export default function useResetPasswordConfirm(uid: string, token: string) {
-	const router = useRouter();
+  const router = useRouter();
 
-	const [resetPasswordConfirm, { isLoading }] =
-		useResetPasswordConfirmMutation();
+  const [resetPasswordConfirm, { isLoading }] =
+    useResetPasswordConfirmMutation();
 
-	const [formData, setFormData] = useState({
-		new_password: '',
-		re_new_password: '',
-	});
+  const [formData, setFormData] = useState({
+    new_password: "",
+    re_new_password: "",
+  });
 
-	const { new_password, re_new_password } = formData;
+  const { new_password, re_new_password } = formData;
 
-	const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-		const { name, value } = event.target;
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
 
-		setFormData({ ...formData, [name]: value });
-	};
+    setFormData({ ...formData, [name]: value });
+  };
 
-	const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-		resetPasswordConfirm({ uid, token, new_password, re_new_password })
-			.unwrap()
-			.then(() => {
-				toast.success('Password reset successful');
-				router.push('/auth/login');
-			})
-			.catch(() => {
-				toast.error('Password reset failed');
-			});
-	};
+    resetPasswordConfirm({ uid, token, new_password, re_new_password })
+      .unwrap()
+      .then(() => {
+        toast.success("Password reset successful");
+        router.push("/auth/login");
+      })
+      .catch((error) => {
+        if (error) {
+          toast.error(`${Object.values(error.data)[0]}`);
+        }
+      });
+  };
 
-	return {
-		new_password,
-		re_new_password,
-		isLoading,
-		onChange,
-		onSubmit,
-	};
+  return {
+    new_password,
+    re_new_password,
+    isLoading,
+    onChange,
+    onSubmit,
+  };
 }
